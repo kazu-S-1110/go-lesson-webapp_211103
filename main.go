d7b65c5e65c2ac5ef361cbab5e7975a105b969c1
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-03 21:46:13 +0900

    configを定義。

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"go-lesson-webapp_211103/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
