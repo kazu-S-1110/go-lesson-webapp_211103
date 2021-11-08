5169514b1a9b53840a205252f3b2f83b342d6e8a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-09 06:14:32 +0900

    postされた値を取得する処理を記述。結構書くことは単純。

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,28 @@
 package controllers
 
-import "net/http"
+import (
+	"go-lesson-webapp_211103/app/models"
+	"log"
+	"net/http"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	// rからのメソッドで処理を分ける
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			Password: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/", 302)
+	}
 }
