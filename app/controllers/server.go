dc185657ffc844a3053ff600e5c14ec4728333b2
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-12 07:52:53 +0900

    delete関数も実装。これで講座は完了！

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -30,7 +30,7 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 	return sess, err
 }
 
-var validPath = regexp.MustCompile("^/todos/(edit|update)/([0-9]+)$")
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9]+)$")
 
 // パターンとして覚える（HandlerFuncを返す関数で関数を引数にとる、高階関数ってやつ
 func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
@@ -63,5 +63,6 @@ func StartMainServer() error {
 	http.HandleFunc("/todos/save", todoSave)
 	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
 	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
